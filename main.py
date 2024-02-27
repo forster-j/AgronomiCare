c8928b096cf3b6e35dff0f7b593732df9f851ebb
Johannes <johannes.forster@me.com>
2024-02-27 17:06:18 +0100

    streamlit_final_release

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import streamlit as st
 from keras.models import load_model
 from PIL import Image
-from keras.preprocessing import image
-import numpy as np
-import io
-import os
-import tempfile
-from util import classify, set_background, nat_recommendation, chem_recommendation
+from util import classify, lookup_value, set_background
 
 # set title
-st.markdown("<h1 style='text-align: center; color: black;'>Agronomicare 🍃</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>Agronomicare</h1>", unsafe_allow_html=True)
 
 # set header
 st.header('Welcome to Agronomicare, your one-stop crop diagnosis and remedy recommender.')
@@ -24,18 +19,36 @@ chemical pesticides.
 '''
 st.markdown(multi)
 
-# set sidebar
-st.sidebar.title('How to: ')
+# center content of sidebar
+
+st.markdown(
+    """
+    <style>
+        [data-testid=stSidebar] [data-testid=stImage]{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+        }
+    </style>
+    """, unsafe_allow_html=True
+)
+
+with st.sidebar:
+    logo = 'images/logo_bg.png'    
+    st.sidebar.image(logo)
+    "# How to:"
 
 # Display the first image in the sidebar
-image1 = 'images/00a79a7b-8b96-452c-91c8-deb54eaa28e5___CREC_HLB 7231.JPG'
-st.sidebar.image(image1, caption= 'uniform background, good focus', width=144)
+image1 = 'images/good_1.png'
+st.sidebar.image(image1, caption= 'uniform background, good focus')
 #Second example image
-image2 = 'images/171026079.jpg'
-st.sidebar.image(image2, caption= 'Please do not capture the whole forest', width=144)
+image2 = 'images/bad_1.png'
+st.sidebar.image(image2, caption= 'Please do not capture the whole forest')
 # Third example image 
-image3 = 'images/redrot (164).jpeg'
-st.sidebar.image(image3, caption= 'Please have all of the leaf in focus', width=144)
+image3 = 'images/bad_2.png'
+st.sidebar.image(image3, caption= 'Please have the whole leaf in focus')
 
 # set some style 
 
@@ -79,24 +92,8 @@ It's good practice to take photos from different angles and consult with your lo
 or botanist to get a reliable prediction.*
 '''
 
-#preprocessing function
-
-def preprocess_image(uploaded_file, target_size=(224, 224)):
-    # Create a temporary file to save the uploaded image
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_file.write(uploaded_file.read())
-
-    # Close the temporary file to ensure it's saved properly
-    temp_file.close()
-
-    # Load the image from the temporary file and resize it to the target size
-    img = image.load_img(temp_file.name, target_size=target_size)
-
-    # Delete the temporary file
-    os.unlink(temp_file.name)
-
-
-file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+st.header("Please upload an image:")
+file = st.file_uploader(label="Choose Photo:", type=["jpg", "jpeg", "png"])
 
 # load model
 model = load_model('models/model_filtered.h5')
@@ -108,46 +105,65 @@ if file is not None:
     # Classify image
     class_name, conf_score = classify(input_image, model, classes)
 
-    # Display predictions
-    st.write("## {}".format(class_name))
-    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
 
     # check if class is 'healthy'
     if class_name == 'healthy':
-        st.write("Congratulations, your crop seems to be healthy!")
+
+        st.header("Congratulations, your crop seems to be healthy!")
         st.write("There is no need for any remedy or pesticide.")
         st.markdown("Happy farming 🚜")
         st.divider()
         st.markdown(disclaimer)
 
     else:
-        st.write(f"Your crop seems to be suffering from {class_name}")
-        # get the recommendation for the natural remmedy
-        recommendation_text_nat = nat_recommendation(class_name)
+        ### Disease name
+        # Get disease name
+        disease_name = lookup_value(class_name, 'disease_name')
+        # Display diesease name
+        st.header(f"This crop suffers from {disease_name}.")
 
-        # Calculate the height of the text area dynamically based on the length of the recommendation text
-        num_lines = max(recommendation_text_nat.count('\n') + 1, 3)  # Adjust minimum height to 3 lines
-        height = num_lines * 20  # 20 pixels per line
+        # Get disease type -- optional since this information is also included in the disease description
+        #disease_type = lookup_value(class_name, 'disease_type')
+        # Display diesease name and type
+        #st.header(f"This crop suffers from {disease_name}, a {disease_type}")
 
-        # Display the recommendation
-        #st.text_area("Natural Remedy Recommendation", value=recommendation_text, height=200)
-        st.markdown("<h2 style='font-weight: bold;'>Natural Remedy Recommendation:</h2>", unsafe_allow_html=True)
-        st.text_area(" ", value=recommendation_text_nat, height=height)
+        ### Disease description
+        # Get disease description
+        disease_description = lookup_value(class_name, 'disease_description')
+        # Calculate height of text area based on the length of the text
+        num_lines = max(disease_description.count('\n') + 1, 3)  # Adjust minimum height to 3 lines
+        height = num_lines * 40  # 40 pixels per line
+        # Display text
+        st.text_area(" ", value=disease_description, height=height)
 
-        # get the recommendation for the chemical remmedy
-        recommendation_text_chem = chem_recommendation(class_name)
+        ### Natural remedy
+        # Get natural remedy
+        natural_remedy = lookup_value(class_name, 'natural_remedies')
+        # Calculate height of text area based on the length of the text
+        num_lines = max(natural_remedy.count('\n') + 1, 3)  # Adjust minimum height to 3 lines
+        height = num_lines * 30  # 30 pixels per line
+        # Display text
+        st.header("Suggested Natural Remedies:")
+        st.text_area(" ", value=natural_remedy, height=height)
 
-        # Calculate the height of the text area dynamically based on the length of the recommendation text
-        num_lines = max(recommendation_text_chem.count('\n') + 1, 3)  # Adjust minimum height to 3 lines
-        height = num_lines * 20  # 20 pixels per line
-
-        # Display the recommendation
-        #st.text_area("Natural Remedy Recommendation", value=recommendation_text, height=200)
-        st.markdown("<h2 style='font-weight: bold;'>Chemical Remedy Recommendation:</h2>", unsafe_allow_html=True)
-        st.text_area(" ", value=recommendation_text_chem, height=height)
+        ### Chemical treatment
+        # Get chemical treatment
+        chemical_treatment = lookup_value(class_name, 'chemical_control')
+        # Calculate height of text area based on the length of the text
+        num_lines = max(chemical_treatment.count('\n') + 1, 3)  # Adjust minimum height to 3 lines
+        height = num_lines * 25  # 30 pixels per line
+        # Display text
+        st.header("Suggested Chemical Treatments:")
+        st.text_area(" ", value=chemical_treatment, height=height)
 
         # Print disclaimer
         st.divider()
         st.markdown(disclaimer)
  
 
+## Just for debugging
+# Display predictions
+# st.write("## {}".format(class_name))
+# st.write("### score: {}%".format(int(conf_score * 1000) / 10))        
+#st.write(f"Disease name: {disease_name}")
+#st.write(f"Disease type: {disease_type}")
